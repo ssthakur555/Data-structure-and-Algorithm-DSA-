aee57545dd03906e24ce601fb67f8a60cd8c7dc5
Shailu Singh Thakur <91122205+ssthakur555@users.noreply.github.com>
2021-10-24 21:38:39 +0530

    Create l Linear Search.py

---
+++ l Linear Search.py
@@ -0,0 +1,8 @@
+def binarySearch(array, target):
+  for i in range(len(lst)):
+    if target==lst[i]:
+        return(target,"is in index number",i)
+    else:
+        return('''it 
+        is not the part of our list ''')
+        break
